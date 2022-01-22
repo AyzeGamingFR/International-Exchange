6a453c5b64ef3571a40d89cbbd0bc99458b1f63b
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-01-22 21:49:06 +0100

    Updated exchange-server.js

--- javascript/nodejs/exchange-server.js
+++ javascript/nodejs/exchange-server.js
@@ -1,32 +1,32 @@
-var fs = require("fs");
-var http = require("http");
-var socket = require("socket");
-var url = require("url");
+const fs = require("fs");
+const http = require("http");
+const socket = require("socket");
+const url = require("url");
 
-var accounts = require("./accounts.edb");
-var logs = require("./logs.txt");
+const accounts_edb = require("./accounts.edb");
+const logs = require("./logs.txt");
 
-var scrypt_a = require("./algos/scrypt.js");
-var scryptd_a = require("./algos/scryptd.js");
-var scryptt_a = require("./algos/scryptt.js");
-var scryptq_a = require("./algos/scryptq.js");
-var sha256_a = require("./algos/sha256.js");
-var sha256d_a = require("./algos/sha256d.js");
-var sha256t_a = require("./algos/sha256t.js");
-var sha256q_a = require("./algos/sha256q.js");
-var randomx_a = require("./algos/randomx.js");
-var x11_a = require("./algos/x11.js");
-var x11d_a = require("./algos/x11d.js");
-var x11t_a = require("./algos/x11t.js");
-var x11q_a = require("./algos/x11q.js");
-var x12_a = require("./algos/x12.js");
-var x12d_a = require("./algos/x12d.js");
-var x12t_a = require("./algos/x12t.js");
-var x12q_a = require("./algos/x12q.js");
-var x13_a = require("./algos/x13.js");
-var x13d_a = require("./algos/x13d.js");
-var x13t_a = require("./algos/x13t.js");
-var x13q_a = require("./algos/x13q.js");
+const scrypt_a = require("./algos/scrypt.js");
+const scryptd_a = require("./algos/scryptd.js");
+const scryptt_a = require("./algos/scryptt.js");
+const scryptq_a = require("./algos/scryptq.js");
+const sha256_a = require("./algos/sha256.js");
+const sha256d_a = require("./algos/sha256d.js");
+const sha256t_a = require("./algos/sha256t.js");
+const sha256q_a = require("./algos/sha256q.js");
+const randomx_a = require("./algos/randomx.js");
+const x11_a = require("./algos/x11.js");
+const x11d_a = require("./algos/x11d.js");
+const x11t_a = require("./algos/x11t.js");
+const x11q_a = require("./algos/x11q.js");
+const x12_a = require("./algos/x12.js");
+const x12d_a = require("./algos/x12d.js");
+const x12t_a = require("./algos/x12t.js");
+const x12q_a = require("./algos/x12q.js");
+conqt x13_a = require("./algos/x13.js");
+const x13d_a = require("./algos/x13d.js");
+const x13t_a = require("./algos/x13t.js");
+const x13q_a = require("./algos/x13q.js");
 
 var cryptocurrencys_listed = ["BTC", "LTC", "MARS", "MOON", "SPACE", "XMR"];
 var cryptocurrencys_listed_database = {
@@ -39,6 +39,20 @@ var cryptocurrencys_listed_database = {
         
     },
     "BTC": {
+        
+        "deposable": 0,
+        "exchangable": 0,
+        "withdrawable": 0
+        
+    },
+    "DASH": {
+        
+        "deposable": 0,
+        "exchangable": 0,
+        "withdrawable": 0
+        
+    },
+    "DOGE": {
         
         "deposable": 0,
         "exchangable": 0,
@@ -119,17 +133,25 @@ function sendRawTransaction(var rawtransaction) {
 
 function withdraw(var cryptocurrency_ticker, var account_hashed_key, var account_hashed_password, var amount, var receiver, var network_fees) {
     
-    if (cryptocurrencys_listed.includes(cryptocurrency_ticker)) {
+    if (cryptocurrency_ticker) {
         
-        if (accounts.includes()) {
+        if (cryptocurrencys_listed.includes(cryptocurrency_ticker)) {
             
+            if (accounts.includes(account_hashed_key)) {
+                
+                
+                
+            };
             
+        } else {
+            
+            return("{'error': 1, 'error_message': 'This cryptocurrency is not in the currencys list !'}");
             
         };
         
     } else {
         
-        return("{'error': 1, 'error_message': 'This cryptocurrency is not in the currencys list !'}");
+        
         
     };
     
