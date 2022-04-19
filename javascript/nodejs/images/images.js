c5d985b7c8c7959a224bdc8327acb537155cc046
AyzeLYC <35877385+AyzeGamingFR@users.noreply.github.com>
2022-04-19 11:18:07 +0200

    Created image.js

---
+++ javascript/nodejs/images/images.js
@@ -0,0 +1,7 @@
+const fs = require("fs");
+
+var logo = fs.readFile("./logo.png");
+
+var homeImage = fs.readFile("./home.png");
+var profileImage = fs.readFile("./profile.png");
+vaar settingsImage = fs.readFile("./settings.png");
